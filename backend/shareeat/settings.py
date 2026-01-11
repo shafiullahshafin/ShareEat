3acbeb8252d9a65dd0c928075f882d4430986ba3
Shafiullah Shafin <shafiullahshafin735@gmail.com>
2026-01-11 14:35:20 +0600

    Fix: Use CompressedStaticFilesStorage to prevent Vercel 500 error

--- backend/shareeat/settings.py
+++ backend/shareeat/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'shareeat' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
